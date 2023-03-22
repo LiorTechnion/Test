0711f6b14a3dc6eb429400ec8a3a793af10b2cb6
liorb <ben.lior@campus.technion.ac.il>
2023-03-22 19:18:10 +0200

    שיניתי את X ל- Y.

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,6 @@
 public class Program {
     public static void main(String[] args) {
-        final byte X = 6;
-        System.out.println(X);
+        final byte Y = 6;
+        System.out.println(Y);
     }
 }
