1a6b0891300abd3e417cca129f3b5fef5a248519
liorb <ben.lior@campus.technion.ac.il>
2023-03-22 19:39:49 +0200

    1. I have split the declaration and the defining of the variable "Y". 2. I have added comments to the code.

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,10 @@
 public class Program {
     public static void main(String[] args) {
-        final byte Y = 6;
+        // declare a variable "Y" as a "final byte"
+        final byte Y;
+        // define the variable "Y" as the number "6"
+        Y = 6;
+        // print the value of the variable "Y"
         System.out.println(Y);
     }
 }
