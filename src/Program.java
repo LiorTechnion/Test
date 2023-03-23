5e336efcfa47ec50f964742abf6806d6a0b9b9da
liorb <ben.lior@campus.technion.ac.il>
2023-03-23 12:33:03 +0200

    TFKUTFKUYFKUY

--- src/Program.java
+++ src/Program.java
@@ -3,7 +3,7 @@ public class Program {
         // declare a variable "Y" as a "final byte"
         byte Y;
         // define the variable "Y" as the number "6"
-        Y = 10;
+        Y = 2;
         // print the value of the variable "Y"
         System.out.println(Y);
     }
