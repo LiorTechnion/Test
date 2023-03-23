2e343d2d3b2920edea2e9d5b41e7f09135ae44fc
Yarin Katan <yarin.katan@campus.technion.ac.il>
2023-03-23 12:24:24 +0200

    fff

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,9 @@
 public class Program {
     public static void main(String[] args) {
         // declare a variable "Y" as a "final byte"
-        final byte Y;
+        byte Y;
         // define the variable "Y" as the number "6"
-        Y = 6;
+        Y = 10;
         // print the value of the variable "Y"
         System.out.println(Y);
     }
